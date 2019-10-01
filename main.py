715d19e625a5b74c79298bf35be0d1a1a55ac5a8
cha0sCat <admin@noob.pw>
2019-10-01 16:01:25 +0800

    人脸匹配改为异步执行

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ Landmarks
 |
 compare with known Faces
 """
+import asyncio
 
 from camera import Camera
 from facedect import FaceDetect
@@ -32,6 +33,7 @@ known_peoples = loadKnownFacesPickle(KNOWN_FACES_PICKLE_PATH)
 
 face_detect = FaceDetect()
 face_recognize = FaceRecognize()
+loop = asyncio.get_event_loop()
 
 
 def initNetwork():
@@ -76,8 +78,43 @@ def unknownFaceMatchSuccess(people_name, face_image, face_node, exist_before=Tru
     saveFaceToFiles(face_image, path)
 
 
+async def runMatch(face_image, face_node):
+    """
+    异步进行面部比对
+    因为numpy面部比对很费时间，比对一次需要0.007s 40次则需要0.28s
+    :param face_image: 人脸图像
+    :param face_node: face_recognize.runImages 的识别结果
+    :return: None
+    """
+    people_name = faceMatch(face_node, known_peoples, FACE_MATCH_THRESHOLD)
+
+    # 认识这个人
+    if people_name:
+        logger.info("Match Faces! {}".format(people_name))
+        knownFaceMatchSuccess(people_name, face_image, face_node)
+
+    # 不认识这个人
+    else:
+        people_name = faceMatch(face_node, unknown_peoples, FACE_MATCH_THRESHOLD)
+
+        # 但是之前见过
+        if people_name:
+            logger.info("Match Unknown Faces {}".format(people_name))
+            unknownFaceMatchSuccess(people_name, face_image, face_node)
+
+        # 根本没见过
+        else:
+            people_name = "unknownPeople_{}".format(genRandomStrings())
+            unknownFaceMatchSuccess(people_name, face_image, face_node, exist_before=False)
+
+
 @timer
 def performOneFrame(frame):
+    """
+    处理一帧图像
+    :param frame: 图像
+    :return: None
+    """
     # 人脸部分图像
     face_images, scores = face_detect.detectFace(frame, crop=True)
 
@@ -85,33 +122,15 @@ def performOneFrame(frame):
     for face_image in face_images:
         # 取得人脸标志点 和数据库内的人脸进行匹配
         face_node = face_recognize.runImages(face_image)
-        people_name = faceMatch(face_node, known_peoples, FACE_MATCH_THRESHOLD)
-
-        # 认识这个人
-        if people_name:
-            logger.info("Match Faces! {}".format(people_name))
-            knownFaceMatchSuccess(people_name, face_image, face_node)
-
-        # 不认识这个人
-        else:
-            people_name = faceMatch(face_node, unknown_peoples, FACE_MATCH_THRESHOLD)
-
-            # 但是之前见过
-            if people_name:
-                logger.info("Match Unknown Faces {}".format(people_name))
-                unknownFaceMatchSuccess(people_name, face_image, face_node)
-
-            # 根本没见过
-            else:
-                people_name = "unknownPeople_{}".format(genRandomStrings())
-                unknownFaceMatchSuccess(people_name, face_image, face_node, exist_before=False)
+        # task = asyncio.create_task(runMatch(face_image, face_node))
+        loop.run_until_complete(runMatch(face_image, face_node))
 
 
 def main():
     initNetwork()
     cam = Camera(CAMERA_ADDRESS)
     while True:
-        logger.debug("new frame")
+        logger.debug("------------------new frame----------------------")
         # 取得图像和
         frame = cam.get_frame()
         frame = imgRotation(frame, -90)
