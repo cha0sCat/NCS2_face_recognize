0f190eb4d6de6972f825b4ca1861be00ad9b82e8
Anonymous <>
2019-10-01 13:43:22 +0800

    增加注释 添加读取数据集功能 优化facedetect结构 增加logging进行日志输出 修改README文档，删除无用数据

--- traning.py
+++ traning.py
@@ -7,7 +7,7 @@
 @Version :   1.0
 @Contact :   ***REMOVED***
 @License :   (C)Copyright 2017-2019, cha0sCat
-@Desc    :   None
+@Desc    :   对识别后保存的未知人脸进行标识并保存到数据集
 """
 import os
 import cv2
@@ -21,7 +21,7 @@ known_peoples = {}
 people_names = os.listdir(IMAGE_SAVE_PATH)
 
 for people_name in people_names:
-    # 初始化此人的面涂特征列表
+    # 初始化此人的面部特征列表
     known_peoples[people_name] = []
     face_image_names = os.listdir(IMAGE_SAVE_PATH + people_name)
     # 对此人名下的所有图片进行识别
